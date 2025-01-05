c772c53435a14821f90593fa215989a1b9325f25
martin gaborit <mgaborit.dev@gmail.com>
2025-01-05 17:08:56 +0100

    feat (db): added recipes and ingredients handling tables

--- db/1_init_db.sql
+++ db/1_init_db.sql
@@ -1,11 +1,178 @@
-CREATE TABLE IF NOT EXISTS public.recipe
-(
-    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
-    name character varying(80) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT "RECIPE_pkey" PRIMARY KEY (id)
-)
+--
+-- PostgreSQL database dump
+--
 
-TABLESPACE pg_default;
+-- Dumped from database version 16.6 (Ubuntu 16.6-0ubuntu0.24.04.1)
+-- Dumped by pg_dump version 16.6 (Ubuntu 16.6-0ubuntu0.24.04.1)
 
-ALTER TABLE IF EXISTS public.recipe
-    OWNER to postgres;
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: recipe; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.recipe (
+    id bigint NOT NULL,
+    name character varying(80) NOT NULL,
+    duration integer,
+    difficulty smallint
+);
+
+
+ALTER TABLE public.recipe OWNER TO postgres;
+
+--
+-- Name: RECIPE_ID_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.recipe ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public."RECIPE_ID_seq"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: ingredient; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.ingredient (
+    id bigint NOT NULL,
+    name character varying(50) NOT NULL,
+    quantity_type smallint NOT NULL
+);
+
+
+ALTER TABLE public.ingredient OWNER TO postgres;
+
+--
+-- Name: ingredient_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.ingredient ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.ingredient_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: recipe_ingredient; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.recipe_ingredient (
+    recipe_id bigint NOT NULL,
+    ingredient_id bigint NOT NULL,
+    quantity integer NOT NULL
+);
+
+
+ALTER TABLE public.recipe_ingredient OWNER TO postgres;
+
+--
+-- Name: recipe_step; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.recipe_step (
+    id bigint NOT NULL,
+    recipe_id bigint NOT NULL,
+    name character varying(80) NOT NULL,
+    description character varying(500)
+);
+
+
+ALTER TABLE public.recipe_step OWNER TO postgres;
+
+--
+-- Name: recipe_step_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.recipe_step ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.recipe_step_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: recipe RECIPE_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.recipe
+    ADD CONSTRAINT "RECIPE_pkey" PRIMARY KEY (id);
+
+
+--
+-- Name: ingredient ingredient_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.ingredient
+    ADD CONSTRAINT ingredient_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: recipe_ingredient recipe_ingredient_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.recipe_ingredient
+    ADD CONSTRAINT recipe_ingredient_pkey PRIMARY KEY (recipe_id, ingredient_id);
+
+
+--
+-- Name: recipe_step recipe_step_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.recipe_step
+    ADD CONSTRAINT recipe_step_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: recipe_ingredient fk_recipe_ingredient_to_ingredient; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.recipe_ingredient
+    ADD CONSTRAINT fk_recipe_ingredient_to_ingredient FOREIGN KEY (ingredient_id) REFERENCES public.ingredient(id);
+
+
+--
+-- Name: recipe_ingredient fk_recipe_ingredient_to_recipe; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.recipe_ingredient
+    ADD CONSTRAINT fk_recipe_ingredient_to_recipe FOREIGN KEY (recipe_id) REFERENCES public.recipe(id);
+
+
+--
+-- Name: recipe_step fk_recipe_step_to_recipe; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.recipe_step
+    ADD CONSTRAINT fk_recipe_step_to_recipe FOREIGN KEY (recipe_id) REFERENCES public.recipe(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ db/dump_init_db_script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pg_dump -s test_db > ~/dev/projects/givemesomepy/db/1_init_db.sql
